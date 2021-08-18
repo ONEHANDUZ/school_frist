babab3c89b2aa029fc7bc3237fd57791df1ba7c0
onehanduz <onehanduz@gmail.com>
2021-08-18 18:55:58 +0500

    auth va dastur skeleti

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('school.home');
+    }
+
+    public function news()
+    {
+        return view('school.news');
+    }
+
+    public function needed()
+    {
+        return view('school.needed');
+    }
+}
